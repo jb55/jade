pugjs/pug

Added a prettyprint option that automatically adds indents and new lines to the compiled html to let it look a little bit more pretty.

lib/parser.js
@@ -30,7 +30,7 @@ var Parser = exports = module.exports = function Parser(str, filename){
  * Tags that may not contain tags.
  */
 
-var textOnly = exports.textOnly = ['pre', 'script', 'textarea', 'style'];
+var textOnly = exports.textOnly = ['pre', 'script', 'textarea', 'style', 'title'];
 
 /**
  * Parser prototype.

jb55/jade

lib/parser.js
@@ -30,7 +30,7 @@ var Parser = exports = module.exports = function Parser(str, filename){
  * Tags that may not contain tags.
  */
 
-var textOnly = exports.textOnly = ['code', 'script', 'textarea', 'style'];
+var textOnly = exports.textOnly = ['code', 'script', 'textarea', 'style', 'title'];
 
 /**
  * Parser prototype.